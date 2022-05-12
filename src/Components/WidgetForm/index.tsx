0de2f85209cc2046d30d742471edb0e864d74241
JoaoBagvanji <jbagvanji54@gmail.com>
2022-05-12 22:10:30 +0200

    adicao de funcionalidades novas

--- src/Components/WidgetForm/index.tsx
+++ src/Components/WidgetForm/index.tsx
@@ -6,6 +6,7 @@ import thoughtImageUrl from '../../assets/thought.svg';
 import { useState } from "react";
 import { FeedbackTypeStep } from '../WidgetForm/Steps/FeedbackTypeStep'
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 
 export const feedBackTypes ={
@@ -39,23 +40,33 @@ export function WidgetForm(){
 
     const [feedbackType , setFeedbackType] = useState<FeedbackType | null>(null)
 
+    const [feedbackSent, setFeedbackSent] = useState(false);
+
 
     function handleRestartFeedback(){
-        setFeedbackType(null)
+        setFeedbackSent(false);
+        setFeedbackType(null);
     }
 
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
-            {!feedbackType ? (
-                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
+            { feedbackSent ? (
+                <FeedbackSuccessStep onFeedbackRestartedRequested={handleRestartFeedback} />
                 
-            ) :(
-                <FeedbackContentStep
-
-                 feedbackType={feedbackType}
-                 onFeedbackRestartRequested={handleRestartFeedback}
-                />
+            ) : (
+                <>
+                    {!feedbackType ? (
+                        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
+                        
+                    ) :(
+                        <FeedbackContentStep
+                            feedbackType={feedbackType}
+                            onFeedbackRestartRequested={handleRestartFeedback}
+                            onFeedbackSent={() => setFeedbackSent(true)}
+                        />
+                    )}
+                </>
             )}
 
             
